cddafc6a063b7dac4a7f735182a4ffadc127c22a
Diogo Pinheiro <diogofg1397@gmail.com>
2018-06-10 12:36:17 +0100

    Finalização dos AlertDialogs para o registo

--- app/src/main/java/com/diogo/fitnesstracker/TerminaRegisto.java
+++ app/src/main/java/com/diogo/fitnesstracker/TerminaRegisto.java
@@ -2,8 +2,12 @@ package com.diogo.fitnesstracker;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.Button;
@@ -22,6 +26,7 @@ public class TerminaRegisto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_termina_registo);
+        botao_done.setEnabled(false);
         editText_height = (EditText) findViewById(R.id.editText_height);
         editText_weight = (EditText) findViewById(R.id.editText_weight);
 
@@ -29,30 +34,7 @@ public class TerminaRegisto extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO inserção do dialogo para a altura
-                final Dialog dialogoAltura = new Dialog(context);
-                dialogoAltura.setContentView(R.layout.costum_dialog);
-                dialogoAltura.setTitle("Height");
-
-                TextView textView_kg = (TextView) dialogoAltura.findViewById(R.id.textView_dialog);
-                textView_kg.setText("Kg");
-                EditText editText_dialogoAltura = (EditText) dialogoAltura.findViewById(R.id.editText_dialog);
-                Button cancelar = (Button) dialogoAltura.findViewById(R.id.botao_cancelar);
-                Button confirmar = (Button) dialogoAltura.findViewById(R.id.botao_confirmar);
-
-                confirmar.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                    }
-                });
-
-                cancelar.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialogoAltura.dismiss();
-                    }
-                });
-                dialogoAltura.show();
+                criaDialogo("Height","cm",editText_height);
             }
         });
 
@@ -60,10 +42,49 @@ public class TerminaRegisto extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO inserção do dialogo para o peso
-
+                criaDialogo("Weight","kg",editText_weight);
             }
         });
     }
 
+    private void criaDialogo(String titulo, String medida, final EditText editText)
+    {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);//obtem um layout xml para uma view
+        View mView = layoutInflater.inflate(R.layout.costum_dialog, null);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setView(mView);
 
+        final EditText editText_dialog = (EditText) mView.findViewById(R.id.editText_dialog);
+        TextView textView_titulo = (TextView)  mView.findViewById(R.id.textview_titulo);
+        TextView textView_medida = (TextView)  mView.findViewById(R.id.textview_medida);
+
+        textView_titulo.setText(titulo);
+        textView_medida.setText(medida);
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+                        //TODO ler dados do utilizador e guardar
+                        editText.setText(editText_dialog.getText());
+                    }
+                })
+
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        final AlertDialog dialog = alertDialogBuilder.create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.parseColor("#39796b"));
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.parseColor("#39796b"));
+            }
+        });
+        dialog.show();
+    }
 }
