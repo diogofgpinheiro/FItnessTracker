fe3e96fe20d7c354a277049e0a65028fd612e61a
Diogo Pinheiro <diogofg1397@gmail.com>
2018-06-27 13:54:13 +0100

    carregamento de fotografias para a firebase carregamento de fotografias da firebase para a foto de perfil finalização da pagina de perfil

--- app/src/main/java/com/diogo/fitnesstracker/Fragments/PerfilFragment.java
+++ app/src/main/java/com/diogo/fitnesstracker/Fragments/PerfilFragment.java
@@ -1,11 +1,19 @@
 package com.diogo.fitnesstracker.Fragments;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +21,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.diogo.fitnesstracker.Manifest;
 import com.diogo.fitnesstracker.R;
 import com.diogo.fitnesstracker.adapter.adapterListaPerfil;
@@ -21,12 +31,22 @@ import com.diogo.fitnesstracker.config.ConfiguracaoFirebase;
 import com.diogo.fitnesstracker.helper.CodificadorBase64;
 import com.diogo.fitnesstracker.model.Perfil;
 import com.diogo.fitnesstracker.model.Utilizador;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,15 +64,18 @@ public class PerfilFragment extends Fragment {
     private DatabaseReference firebaseRef = ConfiguracaoFirebase.getReferenciaFirebase();
     private DatabaseReference perfilRef;
     private ValueEventListener valueEventListenerPerfil;
+    private StorageReference storage = ConfiguracaoFirebase.getFirebaseStorage();
 
     private TextView dadosAltura,dadosPeso,dadosObjetivo,dadosNome,textViewAltura,textViewPeso,textViewObjetivo;
     private ImageView imageViewPerfil;
     private ProgressBar progressBar;
 
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
+
     public PerfilFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,6 +92,50 @@ public class PerfilFragment extends Fragment {
         textViewPeso = v.findViewById(R.id.textViewPeso);
         imageViewPerfil = v.findViewById(R.id.imageView);
 
+        FirebaseUser utilizador = ConfiguracaoFirebase.getAutenticacao().getCurrentUser();
+        Uri url = utilizador.getPhotoUrl();
+
+        if(url != null)
+        {
+            Glide.with(getActivity()).load(url).into(imageViewPerfil);
+        }
+
+
+
+        imageViewPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Escolha uma opção").setItems(R.array.items_fotografia, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //verificar qual das opcoes foi escolhida
+                        switch (which)
+                        {
+                            case 0:
+                                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                if(i.resolveActivity(getActivity().getPackageManager()) != null)
+                                {
+                                startActivityForResult(i, SELECAO_CAMERA); //captura resultado de retorno
+                                }
+                                break;
+                            case 1:
+                                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                if(intent.resolveActivity(getActivity().getPackageManager()) != null)
+                                {
+                                    startActivityForResult(intent, SELECAO_GALERIA); //captura resultado de retorno
+                                }
+                                break;
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
         recyclerView = v.findViewById(R.id.recyclerViewPerfil);
         adapterListaPerfil = new adapterListaPerfil(getContext(),listaPerfil);
@@ -81,6 +148,77 @@ public class PerfilFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         obtemDados();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == getActivity().RESULT_OK)
+        {
+            Bitmap imagem = null;
+
+            try {
+
+                switch (requestCode)
+                {
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+                if(imagem != null)
+                {
+                    imageViewPerfil.setImageBitmap(imagem);
+
+                    //Obter dados da imagem para a firebase
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG,70,baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    // gravar imagem na firebase
+                    String IDUtilizador = CodificadorBase64.codificaBase64(autenticacao.getCurrentUser().getEmail());
+                    StorageReference imagemRef = storage.child("imagens").child("perfil").child(IDUtilizador).child("perfil.jpeg");
+
+                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity(),"Occoreu um erro ao fazer upload da imagem",Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(getActivity(),"Sucesso ao fazer upload da imagem",Toast.LENGTH_SHORT).show();
+                            Uri url = taskSnapshot.getDownloadUrl();
+                            atualizaFoto(url);
+                        }
+                    });
+                }
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void atualizaFoto(Uri url)
+    {
+        FirebaseUser user = ConfiguracaoFirebase.getAutenticacao().getCurrentUser();
+        UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder().setPhotoUri(url).build();
+
+        user.updateProfile(profile).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(!task.isSuccessful())
+                {
+                    Log.d("Perfil","Erro ao atualizar a foto de perfil.");
+                }
+            }
+        });
     }
 
     public void obtemDados()
@@ -126,5 +264,11 @@ public class PerfilFragment extends Fragment {
         textViewPeso.setVisibility(View.VISIBLE);
         imageViewPerfil.setVisibility(View.VISIBLE);
         recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        perfilRef.removeEventListener(valueEventListenerPerfil);
     }
 }
--- app/src/main/java/com/diogo/fitnesstracker/config/ConfiguracaoFirebase.java
+++ app/src/main/java/com/diogo/fitnesstracker/config/ConfiguracaoFirebase.java
@@ -3,11 +3,14 @@ package com.diogo.fitnesstracker.config;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ConfiguracaoFirebase {
 
     private static FirebaseAuth autenticacao;
     private static DatabaseReference referenciaFirebase;
+    private static StorageReference storage;
 
     //retorna a instancia do FirebaseDatabase
     public static DatabaseReference getReferenciaFirebase()
@@ -27,5 +30,14 @@ public class ConfiguracaoFirebase {
             autenticacao = FirebaseAuth.getInstance();
         }
         return autenticacao;
+    }
+
+    public static StorageReference getFirebaseStorage()
+    {
+        if(storage == null)
+        {
+            storage = FirebaseStorage.getInstance().getReference();
+        }
+        return storage;
     }
 }
